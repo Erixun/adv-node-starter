8e2923d3fd68f999921ee557eb77cb28934016f7
erixun <erik.emanuel.sundberg@outlook.com>
2023-04-23 22:19:48 +0200

    log blogs

--- client/src/components/blogs/BlogList.js
+++ client/src/components/blogs/BlogList.js
@@ -7,15 +7,18 @@ import { fetchBlogs } from '../../redux/actions';
 class BlogList extends Component {
   componentDidMount() {
     this.props.fetchBlogs();
+    console.log(this.props.blogs);
   }
 
   renderBlogs() {
-    return map(this.props.blogs, blog => {
+    return map(this.props.blogs, (blog) => {
+      console.log(blog);
       return (
         <div className="card darken-1 horizontal" key={blog._id}>
           <div className="card-stacked">
             <div className="card-content">
               <span className="card-title">{blog.title}</span>
+              <p>{blog._id}</p>
               <p>{blog.content}</p>
             </div>
             <div className="card-action">
