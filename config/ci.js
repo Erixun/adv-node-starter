26b38eb5fde7d6dced10f8bc820bf9adb589d055
erixun <erik.emanuel.sundberg@outlook.com>
2023-05-14 21:56:56 +0200

    add ci.js

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '1043279158277-k6eq2hgncvqgpbcj4tn4jqi48shv3a2p.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI:
+    'mongodb+srv://erixun:PSSN4W8J4npjA4m@cluster0.m6dvoqu.mongodb.net/blog-dev?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
