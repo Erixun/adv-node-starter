f42e66f44f5ece0d6044609bddc444ce475c8bb0
erixun <erik.emanuel.sundberg@outlook.com>
2023-05-14 21:34:47 +0200

    eslint exceptions

--- client/src/components/blogs/formFields.js
+++ client/src/components/blogs/formFields.js
@@ -1,4 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   { label: 'Blog Title', name: 'title' },
-  { label: 'Content', name: 'content' }
+  { label: 'Content', name: 'content' },
 ];
